05214fd78db5ced5a5fe2794cfd70bbf7cb2d594
inphi <mlaw2501@gmail.com>
2022-07-28 15:03:12 -0400

    revise gen-genesis parameters

--- genesis-gen/genssz.sh
+++ genesis-gen/genssz.sh
@@ -7,9 +7,15 @@ GETH_DATADIR=$(mktemp -d)
 BEACON_GEN=./gen-beacon-genesis
 BEACON_GEN_EDIT=./beacon-genesis-edit
 
+# We have 5 mins to setup the beacon node after generation
+GENESIS_TIME=$(($(date '+%s')+300))
+
 ${GETH} --datadir ${GETH_DATADIR} init ./geth_genesis.json
 GENESIS_HASH=$($GETH --datadir ${GETH_DATADIR} console --exec 'web3.eth.getBlockByNumber("0x0")["hash"]' | sed 's/"//g')
-${GETH} --datadir ${GETH_DATADIR} console --exec 'web3.eth.getBlockByNumber("0x0")["hash"]'
-${BEACON_GEN} --output-ssz=./genesis.tmp.ssz --deposit-json-file ./deposit-data-1657723220.json --config-name eip4844
+${BEACON_GEN}\
+    --output-ssz=./genesis.tmp.ssz\
+    --deposit-json-file ./deposit-data-1657723220.json\
+    --genesis-time ${GENESIS_TIME}\
+    --config-name eip4844
 ${BEACON_GEN_EDIT} -file ./genesis.tmp.ssz -eth1-blockhash ${GENESIS_HASH}
 mv ./genesis.tmp.ssz ./genesis.ssz
