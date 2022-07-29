1e72fbf7be9d54f17c3d06605c656cf6a2a31608
inphi <mlaw2501@gmail.com>
2022-07-29 01:02:46 -0400

    increase GENESIS_TIME delay

--- genesis-gen/genssz.sh
+++ genesis-gen/genssz.sh
@@ -7,8 +7,8 @@ GETH_DATADIR=$(mktemp -d)
 BEACON_GEN=./gen-beacon-genesis
 BEACON_GEN_EDIT=./beacon-genesis-edit
 
-# We have 5 mins to setup the beacon node after generation
-GENESIS_TIME=$(($(date '+%s')+300))
+# We have 8 mins to setup the beacon node after generation
+GENESIS_TIME=$(($(date '+%s')+500))
 
 ${GETH} --datadir ${GETH_DATADIR} init ./geth_genesis.json
 GENESIS_HASH=$($GETH --datadir ${GETH_DATADIR} console --exec 'web3.eth.getBlockByNumber("0x0")["hash"]' | sed 's/"//g')
