b1f52ce06b6259964338b947af31b8a039b9399b
inphi <mlaw2501@gmail.com>
2022-10-06 16:41:32 -0400

    beacon_gen is no more. replace with prysmctl

--- genesis-gen/genssz.sh
+++ genesis-gen/genssz.sh
@@ -6,6 +6,7 @@ GETH=./geth
 GETH_DATADIR=$(mktemp -d)
 BEACON_GEN=./gen-beacon-genesis
 BEACON_GEN_EDIT=./beacon-genesis-edit
+PRYSMCTL=./prysmctl
 
 # We have 8 mins to setup the beacon node after generation
 # The beacon chain must be up and running before then to ensure that the 4844 upgrade occurs in time - during the slot the EL hardforks
@@ -13,10 +14,11 @@ GENESIS_TIME=$(($(date '+%s')+500))
 
 ${GETH} --datadir ${GETH_DATADIR} init ./geth_genesis.json
 GENESIS_HASH=$($GETH --datadir ${GETH_DATADIR} console --exec 'web3.eth.getBlockByNumber("0x0")["hash"]' | sed 's/"//g')
-${BEACON_GEN}\
+${PRYSMCTL} testnet generate-genesis\
     --output-ssz=./genesis.tmp.ssz\
     --deposit-json-file ./deposit_data.json\
     --genesis-time ${GENESIS_TIME}\
-    --config-name eip4844
+    --config-name eip4844\
+    --num-validators 0
 ${BEACON_GEN_EDIT} -file ./genesis.tmp.ssz -eth1-blockhash ${GENESIS_HASH}
 mv ./genesis.tmp.ssz ./genesis.ssz
